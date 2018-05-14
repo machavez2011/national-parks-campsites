e5c7cc25627361b7ed74bec0e5b5641bdfc5cee3
Michael Chavez <machavez2011@gmail.com>
2018-05-14 15:12:01 -0700

    Started the navbar and added a home page component

--- content/src/App.js
+++ content/src/App.js
@@ -1,12 +1,26 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import "./App.css";
+
+import Home from "./components/Home";
 import Contacts from "./components/Contacts";
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <Contacts />
+        <BrowserRouter>
+          <div>
+            <div>
+              <Link to="/home" className='btn btn-link navbarBtn'>Home</Link>
+              <Link to="/contacts" className='btn btn-link navbarBtn'>Contacts</Link>
+            </div>
+            <div>
+              <Route path="/home" component={Home} />
+              <Route path="/contacts" component={Contacts} />
+            </div>
+          </div>
+        </BrowserRouter>
       </React.Fragment>
     );
   }
---
+++ content/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Home extends React.PureComponent {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Home Page</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Home;
