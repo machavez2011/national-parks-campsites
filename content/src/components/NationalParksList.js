25840abe3d1b75125b0cafb62b62fb3e523398e6
Michael Chavez <machavez2011@gmail.com>
2018-05-22 15:29:10 -0700

    Made code DRY by looping through states array for header tag and using the same value to check the national park for that state in the ul

--- content/src/components/NationalParksList.js
+++ content/src/components/NationalParksList.js
@@ -1,13 +1,42 @@
 import React from "react";
 
-// const border = {
-//   border: "solid",
-//   marginTop: "10px",
-//   marginLeft: "10px"
-// };
-
 class NationalParksList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      state: ["California", "Washington", "Nevada"]
+    };
+  }
   render() {
+    const statesAndNationalParks = this.state.state
+      ? this.state.state.map(state => (
+          <React.Fragment>
+            <h3>{state}</h3>
+            <ul>
+              {this.props.nationalParks
+                ? this.props.nationalParks.map(nationalPark => (
+                    <React.Fragment>
+                      {nationalPark.state === state ? (
+                        <div className="col-md-12">
+                          <li
+                            key={nationalPark._id}
+                            onClick={this.props.onClick.bind(
+                              this,
+                              nationalPark
+                            )}
+                          >
+                            {nationalPark.nationalPark}
+                          </li>
+                        </div>
+                      ) : null}
+                    </React.Fragment>
+                  ))
+                : null}
+            </ul>
+          </React.Fragment>
+        ))
+      : null;
+
     const CaliforniaNationalParks = this.props.nationalParks ? (
       this.props.nationalParks.map(nationalPark => (
         <React.Fragment>
@@ -64,12 +93,14 @@ class NationalParksList extends React.Component {
     );
     return (
       <React.Fragment>
-        <h3>California</h3>
+        {/* <h3>California</h3>
         {CaliforniaNationalParks}
         <h3>Nevada</h3>
         {NevadaNationalParks}
         <h3>Washington</h3>
-        {WashingtonNationalParks}
+        {WashingtonNationalParks} */}
+
+        {statesAndNationalParks}
       </React.Fragment>
     );
   }
