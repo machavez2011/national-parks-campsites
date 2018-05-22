ece6fe4599159021de5a3c0be1bc7b2f79c034af
Michael Chavez <machavez2011@gmail.com>
2018-05-22 15:32:09 -0700

    Removed excess WET code

--- content/src/components/NationalParksList.js
+++ content/src/components/NationalParksList.js
@@ -36,73 +36,7 @@ class NationalParksList extends React.Component {
           </React.Fragment>
         ))
       : null;
-
-    const CaliforniaNationalParks = this.props.nationalParks ? (
-      this.props.nationalParks.map(nationalPark => (
-        <React.Fragment>
-          {nationalPark.state === "California" ? (
-            <div className="col-md-12">
-              <li
-                key={nationalPark._id}
-                onClick={this.props.onClick.bind(this, nationalPark)}
-              >
-                {nationalPark.nationalPark}
-              </li>
-            </div>
-          ) : null}
-        </React.Fragment>
-      ))
-    ) : (
-      <React.Fragment />
-    );
-    const NevadaNationalParks = this.props.nationalParks ? (
-      this.props.nationalParks.map(nationalPark => (
-        <React.Fragment>
-          {nationalPark.state === "Nevada" ? (
-            <div className="col-md-12">
-              <li
-                key={nationalPark._id}
-                onClick={this.props.onClick.bind(this, nationalPark)}
-              >
-                {nationalPark.nationalPark}
-              </li>
-            </div>
-          ) : null}
-        </React.Fragment>
-      ))
-    ) : (
-      <React.Fragment />
-    );
-    const WashingtonNationalParks = this.props.nationalParks ? (
-      this.props.nationalParks.map(nationalPark => (
-        <React.Fragment>
-          {nationalPark.state === "Washington" ? (
-            <div className="col-md-12">
-              <li
-                key={nationalPark._id}
-                onClick={this.props.onClick.bind(this, nationalPark)}
-              >
-                {nationalPark.nationalPark}
-              </li>
-            </div>
-          ) : null}
-        </React.Fragment>
-      ))
-    ) : (
-      <React.Fragment />
-    );
-    return (
-      <React.Fragment>
-        {/* <h3>California</h3>
-        {CaliforniaNationalParks}
-        <h3>Nevada</h3>
-        {NevadaNationalParks}
-        <h3>Washington</h3>
-        {WashingtonNationalParks} */}
-
-        {statesAndNationalParks}
-      </React.Fragment>
-    );
+    return <React.Fragment>{statesAndNationalParks}</React.Fragment>;
   }
 }
 
